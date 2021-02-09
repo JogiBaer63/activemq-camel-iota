b01bb63440ae1ff1330ecd3b7073336c2ffe9fa4
Creayl <nikoback93@gmail.com>
2021-02-09 14:01:05 +0100

    first commit

---
+++ src/main/java/de/pickert/activemq_camel/iota/MySpringBootApplication.java
@@ -0,0 +1,31 @@
+package de.pickert.activemq_camel.iota;
+
+import org.apache.camel.CamelContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class MySpringBootApplication implements CommandLineRunner {
+
+	@Autowired
+	private CamelContext camelContext;
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringBootApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Starting Application...");
+	}
+
+	@Bean
+	public MySpringBootRouter msgRouteBuilder() throws Exception {
+		MySpringBootRouter routeBuilder = new MySpringBootRouter();
+		camelContext.addRoutes(routeBuilder);
+		return routeBuilder;
+	}
+}
